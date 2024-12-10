a391a251606580e62222001b21fbc5e667179cc9
Andrey Meshkov <am@adguard.com>
2024-12-10 18:56:45 +0300

    Added iOS app

---
+++ safari-blocker-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  safari-blocker-ios
+//
+//  Created by Andrey Meshkov on 10/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ safari-blocker-ios/safari_blocker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  safari_blocker_iosApp.swift
+//  safari-blocker-ios
+//
+//  Created by Andrey Meshkov on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct safari_blocker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
