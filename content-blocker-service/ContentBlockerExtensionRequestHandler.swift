990d46d7b195596d170c709c056c78819158b564
Andrey Meshkov <am@adguard.com>
2025-04-11 11:39:44 +0300

    added surfrules

--- content-blocker-service/ContentBlockerExtensionRequestHandler.swift
+++ content-blocker-service/ContentBlockerExtensionRequestHandler.swift
@@ -8,37 +8,37 @@
 /// Implements content blocker extension logic.
 /// TODO(ameshkov): Write better comment
 public class ContentBlockerExtensionRequestHandler {
-    
+
     /// Handles content blocking extension request for rules.
     public static func handleRequest(with context: NSExtensionContext, groupIdentifier: String) {
         NSLog("Start loading the content blocker")
-        
+
         // Get the shared container URL.
         guard let appGroupURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupIdentifier) else {
             context.cancelRequest(withError: createError(code: 1001, message: "Failed to access App Group container."))
             return
         }
-        
+
         let sharedFileURL = appGroupURL.appendingPathComponent(Constants.SAFARI_BLOCKER_FILE_NAME)
 
         var blockerListFileURL = sharedFileURL
         if !FileManager.default.fileExists(atPath: sharedFileURL.path) {
             NSLog("No blocker list file found. Using the default one.")
-            
+
             blockerListFileURL = Bundle.main.url(forResource: "blockerList", withExtension: "json")!
         }
-        
+
         let attachment = NSItemProvider(contentsOf: blockerListFileURL)!
-        
+
         let item = NSExtensionItem()
         item.attachments = [attachment]
-        
+
         context.completeRequest(returningItems: [ item ],
                                 completionHandler: { _ in
             NSLog("Finished loading the content blocker")
         })
     }
-    
+
     private static func createError(code: Int, message: String) -> NSError {
         return NSError(
             domain: "dev.adguard.safari-blocker",
