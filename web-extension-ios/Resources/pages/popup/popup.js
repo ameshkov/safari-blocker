11584429e62fc2c625eb9fb606c98594461d237e
Andrey Meshkov <am@adguard.com>
2025-04-17 19:57:32 +0300

    Switch to release version of SafariConverterLib

--- web-extension-ios/Resources/pages/popup/popup.js
+++ web-extension-ios/Resources/pages/popup/popup.js
@@ -1,5 +1,5 @@
 /*
- * WebExtension v1.0.0 (build date: Thu, 17 Apr 2025 16:22:28 GMT)
+ * WebExtension v1.0.0 (build date: Thu, 17 Apr 2025 16:47:49 GMT)
  * (c) 2025 ameshkov
  * Released under the ISC license
  * https://github.com/ameshkov/safari-blocker
